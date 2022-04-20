c1ba7f2d6507cf2c4b0cd54c426bfbfbb6094cc6
David Crespo <crespo.dm@gmail.com>
2022-04-20 17:26:21 -0500

    repo extremely wild error message due to extracting table component

--- src/main.tsx
+++ src/main.tsx
@@ -1,158 +1,169 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
-import './index.css'
+import "./index.css";
 
+import type { TableInstance } from "@tanstack/react-table";
 import {
   createTable,
   getCoreRowModelSync,
   useTableInstance,
-} from '@tanstack/react-table'
+} from "@tanstack/react-table";
 
 type Person = {
-  firstName: string
-  lastName: string
-  age: number
-  visits: number
-  status: string
-  progress: number
-}
+  firstName: string;
+  lastName: string;
+  age: number;
+  visits: number;
+  status: string;
+  progress: number;
+};
 
 const defaultData: Person[] = [
   {
-    firstName: 'tanner',
-    lastName: 'linsley',
+    firstName: "tanner",
+    lastName: "linsley",
     age: 24,
     visits: 100,
-    status: 'In Relationship',
+    status: "In Relationship",
     progress: 50,
   },
   {
-    firstName: 'tandy',
-    lastName: 'miller',
+    firstName: "tandy",
+    lastName: "miller",
     age: 40,
     visits: 40,
-    status: 'Single',
+    status: "Single",
     progress: 80,
   },
   {
-    firstName: 'joe',
-    lastName: 'dirte',
+    firstName: "joe",
+    lastName: "dirte",
     age: 45,
     visits: 20,
-    status: 'Complicated',
+    status: "Complicated",
     progress: 10,
   },
-]
+];
 
-const table = createTable<{ Row: Person }>()
+export type TableProps<D> = {
+  instance: TableInstance<{ Row: D }>;
+};
+
+export function Table<D>({ instance }: TableProps<D>) {
+  return (
+    <table {...instance.getTableProps()}>
+      <thead>
+        {instance.getHeaderGroups().map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((header) => (
+              <th {...header.getHeaderProps()}>
+                {header.isPlaceholder ? null : header.renderHeader()}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...instance.getTableBodyProps()}>
+        {instance.getRowModel().rows.map((row) => (
+          <tr {...row.getRowProps()}>
+            {row.getVisibleCells().map((cell) => (
+              <td {...cell.getCellProps()}>{cell.renderCell()}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+      <tfoot>
+        {instance.getFooterGroups().map((footerGroup) => (
+          <tr {...footerGroup.getFooterGroupProps()}>
+            {footerGroup.headers.map((header) => (
+              <th {...header.getFooterProps()}>
+                {header.isPlaceholder ? null : header.renderFooter()}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </tfoot>
+    </table>
+  );
+}
+
+const table = createTable<{ Row: Person }>();
 
 const defaultColumns = table.createColumns([
   table.createGroup({
-    header: 'Name',
-    footer: props => props.column.id,
+    header: "Name",
+    footer: (props) => props.column.id,
     columns: [
-      table.createDataColumn('firstName', {
-        cell: info => info.value,
-        footer: props => props.column.id,
+      table.createDataColumn("firstName", {
+        cell: (info) => info.value,
+        footer: (props) => props.column.id,
       }),
-      table.createDataColumn(row => row.lastName, {
-        id: 'lastName',
-        cell: info => info.value,
+      table.createDataColumn((row) => row.lastName, {
+        id: "lastName",
+        cell: (info) => info.value,
         header: () => <span>Last Name</span>,
-        footer: props => props.column.id,
+        footer: (props) => props.column.id,
       }),
     ],
   }),
   table.createGroup({
-    header: 'Info',
-    footer: props => props.column.id,
+    header: "Info",
+    footer: (props) => props.column.id,
     columns: [
-      table.createDataColumn('age', {
-        header: () => 'Age',
-        footer: props => props.column.id,
+      table.createDataColumn("age", {
+        header: () => "Age",
+        footer: (props) => props.column.id,
       }),
       table.createGroup({
-        header: 'More Info',
+        header: "More Info",
         columns: [
-          table.createDataColumn('visits', {
+          table.createDataColumn("visits", {
             header: () => <span>Visits</span>,
-            footer: props => props.column.id,
+            footer: (props) => props.column.id,
           }),
-          table.createDataColumn('status', {
-            header: 'Status',
-            footer: props => props.column.id,
+          table.createDataColumn("status", {
+            header: "Status",
+            footer: (props) => props.column.id,
           }),
-          table.createDataColumn('progress', {
-            header: 'Profile Progress',
-            footer: props => props.column.id,
+          table.createDataColumn("progress", {
+            header: "Profile Progress",
+            footer: (props) => props.column.id,
           }),
         ],
       }),
     ],
   }),
-])
+]);
 
 function App() {
-  const [data, setData] = React.useState(() => [...defaultData])
+  const [data, setData] = React.useState(() => [...defaultData]);
   const [columns] = React.useState<typeof defaultColumns>(() => [
     ...defaultColumns,
-  ])
+  ]);
 
-  const rerender = React.useReducer(() => ({}), {})[1]
+  const rerender = React.useReducer(() => ({}), {})[1];
 
   const instance = useTableInstance(table, {
     data,
     columns,
     getCoreRowModel: getCoreRowModelSync(),
-  })
+  });
 
   return (
     <div className="p-2">
-      <table {...instance.getTableProps()}>
-        <thead>
-          {instance.getHeaderGroups().map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(header => (
-                <th {...header.getHeaderProps()}>
-                  {header.isPlaceholder ? null : header.renderHeader()}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...instance.getTableBodyProps()}>
-          {instance.getRowModel().rows.map(row => (
-            <tr {...row.getRowProps()}>
-              {row.getVisibleCells().map(cell => (
-                <td {...cell.getCellProps()}>{cell.renderCell()}</td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-        <tfoot>
-          {instance.getFooterGroups().map(footerGroup => (
-            <tr {...footerGroup.getFooterGroupProps()}>
-              {footerGroup.headers.map(header => (
-                <th {...header.getFooterProps()}>
-                  {header.isPlaceholder ? null : header.renderFooter()}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </tfoot>
-      </table>
+      <Table instance={instance} />
       <div className="h-4" />
       <button onClick={() => rerender()} className="border p-2">
         Rerender
       </button>
     </div>
-  )
+  );
 }
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root"),
+);
